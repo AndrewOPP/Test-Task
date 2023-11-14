b0d09577731c16b182ab0f9f2e2ca369f83c8417
AndrewOPP <a.vyblov2003@gmail.com>
2023-11-14 11:49:52 +0300

    done

--- main.js
+++ main.js
@@ -1,54 +1,64 @@
-import { createMarkUp } from "./markUp.js";
+import { gameMarkUp } from "./markUp.js";
 
-createMarkUp();
+class Game {
+  constructor() {
+    this.gameField = document.querySelector(".gameField");
+    this.allFieldArr = [...this.gameField.children];
+    this.NumberOfLine = Math.floor(
+      this.gameField.offsetWidth / this.allFieldArr[0].offsetWidth
+    );
+    this.gameField.addEventListener(
+      "click",
+      this.fieldsWhichIsSelected.bind(this)
+    );
+  }
 
-const mainField = document.querySelector(".gameField");
-const allElemArr = [...mainField.children]; // Массив всех игровых ячеек
-const NumberOfLine = Math.floor(
-  mainField.offsetWidth / allElemArr[0].offsetWidth // Находим количество игровых ячеек в ряду
-);
+  fieldsWhichIsSelected(e) {
+    const element = e.target.closest("div");
+    if (!element || element.classList.contains("gameField")) return;
 
-mainField.addEventListener("click", selectedFields);
-
-//Основная функция
-function selectedFields(e) {
-  const target = e.target.closest("div");
-  if (target.classList.contains("gameField")) return;
-
-  target.classList.add("alreadyPicked");
-  isCloseElementEqual(allElemArr.indexOf(target), target.id, target);
-  target.textContent = "";
-}
-
-//Функция для проверки соседних  игровых ячеек
-function isCloseElementEqual(elementIndex, elementTextContent, element) {
-  let nextElem = calcElem(elementIndex + 1);
-  if (element.offsetTop !== nextElem.offsetTop) nextElem = `<p></p>`;
-  let prevElem = calcElem(elementIndex - 1);
-  if (element.offsetTop !== prevElem.offsetTop) prevElem = `<p></p>`;
-  const aboveElem = calcElem(elementIndex + NumberOfLine);
-  const botElem = calcElem(elementIndex - NumberOfLine);
-
-  changeCloseELem(nextElem, elementTextContent);
-  changeCloseELem(prevElem, elementTextContent);
-  changeCloseELem(aboveElem, elementTextContent);
-  changeCloseELem(botElem, elementTextContent);
-}
-
-//Функция обозначения конкретного соседней игровой ячейки
-function calcElem(elemIndex) {
-  return allElemArr[elemIndex] ? allElemArr[elemIndex] : "";
-}
-
-//Функция для проверки конкретного соседней игровой ячейки
-function changeCloseELem(element, elementTextContent) {
-  if (
-    element.id === elementTextContent &&
-    !element.classList.contains("alreadyPicked")
-  ) {
-    element.classList.add("alreadyPicked");
-    isCloseElementEqual(allElemArr.indexOf(element), element.id, element);
-
+    element.classList.add("alreadyDelete");
+    this.isCloseElementEqual(
+      this.allFieldArr.indexOf(element),
+      element.id,
+      element
+    );
     element.textContent = "";
   }
+
+  findClosestElems(elemIndex) {
+    return this.allFieldArr[elemIndex];
+  }
+
+  changeClosestElems(element, elementTextContent) {
+    if (
+      element &&
+      element.id === elementTextContent &&
+      !element.classList.contains("alreadyDelete")
+    ) {
+      element.classList.add("alreadyDelete");
+      this.isCloseElementEqual(
+        this.allFieldArr.indexOf(element),
+        element.id,
+        element
+      );
+      element.textContent = "";
+    }
+  }
+
+  isCloseElementEqual(elementIndex, textContent, element) {
+    const aboveElem = this.findClosestElems(elementIndex + this.NumberOfLine);
+    const botElem = this.findClosestElems(elementIndex - this.NumberOfLine);
+    let nextElem = this.findClosestElems(elementIndex + 1);
+    if (nextElem && element.offsetTop !== nextElem.offsetTop) nextElem = null;
+    let prevElem = this.findClosestElems(elementIndex - 1);
+    if (prevElem && element.offsetTop !== prevElem.offsetTop) prevElem = null;
+
+    const closeElemArr = [aboveElem, botElem, nextElem, prevElem];
+    closeElemArr.forEach((element) => {
+      this.changeClosestElems(element, textContent);
+    });
+  }
 }
+
+const game = new Game();
--- markUp.js
+++ markUp.js
@@ -1,53 +1,60 @@
-export function createMarkUp() {
-  const mainField = document.querySelector(".gameField");
-  let markup = [];
-
-  const createMark = () => {
-    randomEl();
-    for (let index = 1; index <= 30; index++) {
-      const { id, svg } = randomEl();
-      markup.push(`
-        <div id=${id} class="elementField">
-            <svg class="svgIcons" width="60px" height="60px">
-                <use href="./sprite.svg#${svg}"></use>
-            </svg>
-        </div>`);
-    }
-    markup = markup.join("");
-  };
-  createMark();
-
-  mainField.insertAdjacentHTML("beforeend", markup);
-}
-
-function randomEl() {
-  let id;
-  let svg;
-
-  const randomNum = Math.floor(Math.random() * 4);
-
-  switch (randomNum) {
-    case 1:
-      svg = "icon-heart";
-      id = 1;
-      break;
-    case 2:
-      svg = "icon-diamond";
-      id = 2;
-      break;
-    case 3:
-      svg = "icon-club";
-      id = 3;
-      break;
-
-    default:
-      svg = "icon-spade";
-      id = 0;
-      break;
+class GameFieldMarkup {
+  constructor() {
+    this.gameField = document.querySelector(".gameField");
   }
 
-  return {
-    svg: svg,
-    id: id,
-  };
+  createMarkup() {
+    let mainMarkup = [];
+    const createMark = () => {
+      this.randomEl();
+      for (let index = 1; index <= 42; index++) {
+        const { id, svgIcon } = this.randomEl();
+        mainMarkup.push(`
+          <div  class="elementField" id=${id}>
+              <svg class="svgIcons" height="60px" width="60px">
+                  <use href="./sprite.svg#${svgIcon}"></use>
+              </svg>
+          </div>`);
+      }
+      mainMarkup = mainMarkup.join("");
+    };
+    createMark();
+
+    this.gameField.insertAdjacentHTML("beforeend", mainMarkup);
+  }
+
+  randomEl() {
+    let svgIcon;
+    let id;
+
+    const randomField = Math.floor(Math.random() * 4);
+
+    switch (randomField) {
+      case 1:
+        svgIcon = "icon-heart";
+        id = 1;
+        break;
+      case 2:
+        svgIcon = "icon-diamond";
+        id = 2;
+        break;
+      case 3:
+        svgIcon = "icon-club";
+        id = 3;
+        break;
+
+      default:
+        svgIcon = "icon-spade";
+        id = 0;
+        break;
+    }
+
+    return {
+      svgIcon: svgIcon,
+      id: id,
+    };
+  }
 }
+
+export const gameMarkUp = new GameFieldMarkup();
+gameMarkUp.createMarkup();
